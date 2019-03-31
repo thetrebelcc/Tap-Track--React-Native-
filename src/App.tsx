78eab9433cb76cd77e2b6f77af2a3ddf2d694739
Fabian Anguiano <fabiananguiano@gmail.com>
2019-03-30 23:18:53 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+const App: React.FC = () => {
+  
+    return (
+      <View>
+<Text>Hello from react nati e</Text>
+      </View>
+      );
+};
+
+export default App;
